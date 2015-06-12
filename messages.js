5e1bc5144441c818427cce5aadef0026dd228434
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-06-12 16:10:12 -0500

    Removing unneeded files

--- messages.js
+++
@@ -1,8 +0,0 @@
-//For messages
-var messages = {};
-//ES
-messages.es.constraints.email = "El email es obligatorio";
-messages.es.constraints.name = "El nombre es obligatorio";
-messages.es.constraints.lastName = "El nombre es obligatorio";
-//EN
-module.export =  messages;
