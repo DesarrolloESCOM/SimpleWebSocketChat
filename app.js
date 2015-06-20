fdd812ac8c2d0399f9a3c861092a466cc592e488
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-06-20 18:37:36 -0500

    Reformating code

--- app.js
+++ app.js
@@ -20,48 +20,48 @@ app.set('view engine', 'jade');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 // multer file management
 app.use(multer({
-    dest:'./public/uploads/',
-    rename: function(fieldname, filename){
-        return filename+Date.now()+parseInt((171)*Math.random())
-    },
-    onFileUploadStart: function(file){
-        console.log("A new file is being added");
-    },
-    onFileUploadComplete: function(file){
-        var socketIO = require('socket.io-client');
-        var messageBot = socketIO.connect('http://localhost:3000/messageNamespace');
-        var data = {};
-        data.user = 'ServerBot';
-        data.content="A new file has been uploaded!!!"
-        data.date = Date.now();
-        // Adding a message to the chat about the image
-        console.log(data);
-        console.log(messageBot);
-        console.log(messageBot.id);
-        messageBot.emit('message', data);
-        done=true;
-    }
+  dest: './public/uploads/',
+  rename: function (fieldname, filename) {
+    return filename + Date.now() + parseInt((171) * Math.random())
+  },
+  onFileUploadStart: function (file) {
+    console.log("A new file is being added");
+  },
+  onFileUploadComplete: function (file) {
+    var socketIO = require('socket.io-client');
+    var messageBot = socketIO.connect('http://localhost:3000/messageNamespace');
+    var data = {};
+    data.user = 'ServerBot';
+    data.content = "A new file has been uploaded!!!"
+    data.date = Date.now();
+    // Adding a message to the chat about the image
+    console.log(data);
+    console.log(messageBot);
+    console.log(messageBot.id);
+    messageBot.emit('message', data);
+    done = true;
+  }
 }));
-app.post('/fileUpload',function(req,res){
-    if(done==true){
-        res.end("File has been uploaded");
-    }
+app.post('/fileUpload', function (req, res) {
+  if (done == true) {
+    res.end("File has been uploaded");
+  }
 });
-app.get('/getFile',function(req,res){
-    console.log("Get a file");
+app.get('/getFile', function (req, res) {
+  console.log("Get a file");
 });
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
@@ -69,22 +69,22 @@ app.use(function (req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 module.exports = app;
--- sockets/sockets.js
+++ sockets/sockets.js
@@ -1,13 +1,14 @@
 var iolib = require('socket.io');
 //creating the prototype
-function socketManagement () {};
+function socketManagement() {
+};
 
-socketManagement.prototype.createSocket = function(serverInstance, callback){
-	try{
-		this.socket = iolib.listen(serverInstance);	
-		callback(null,this.socket);
-	} catch (err){
-		callback(err,null);
-	}
+socketManagement.prototype.createSocket = function (serverInstance, callback) {
+  try {
+    this.socket = iolib.listen(serverInstance);
+    callback(null, this.socket);
+  } catch (err) {
+    callback(err, null);
+  }
 }
 module.exports = socketManagement;
