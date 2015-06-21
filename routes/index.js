8c2473d46c85687eb088aafa5d25507669454b21
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-06-20 23:30:06 -0500

    Adding Mime type service

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
-
+var mime = require('mime');
 /* GET home page. */
 router.get('/', function (req, res) {
   res.render('index', {title: 'Express'});
+});
+router.get('/getMimeType/:file',function(req, res){
+  var mimeType = mime.lookup('./public/upload/'+req.params.file);
+  if(mimeType){
+    res.json({'Content-Type': mimeType});		
+  } else {
+  	res.json({'Content-Type': 'octet-stream'});
+  }
 });
 module.exports = router;
