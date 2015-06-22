d54ef914b9e49b465de3ab7d83cff02533183ab0
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-06-21 22:45:48 -0500

    Updating routes

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,35 @@
 var express = require('express');
 var router = express.Router();
 var mime = require('mime');
+var fs = require('fs');
 /* GET home page. */
 router.get('/', function (req, res) {
   res.render('index', {title: 'Express'});
 });
-router.get('/getMimeType/:file',function(req, res){
-  var mimeType = mime.lookup('./public/upload/'+req.params.file);
+router.get('/getMimeType',function(req, res){
+  var mimeType = mime.lookup('./public/uploads/'+req.query.nameOfFile);
   if(mimeType){
     res.json({'Content-Type': mimeType});		
   } else {
   	res.json({'Content-Type': 'octet-stream'});
   }
+});
+router.get('/getFileLog', function(req, res){
+  var files = fs.readdirSync('./public/uploads/');
+  /*
+  var filteredFiles = [];
+  for(var i=0;i<files.length;i++){
+    var fileInformation = fs.statSync(files[i]);
+    if(!(fileInformation.isDirectory())){
+      console.log(files[i]);
+      console.log(files[i]);
+      console.log(files[i]);
+  	  filteredFiles.push(files[i]);
+    }
+    res.json(filteredFiles);
+    return;
+  }
+  */
+  res.json(files);
 });
 module.exports = router;
